3bd5b0830616f860d123014354c087ac150a1093
Jose <joseesapel@gmail.com>
2025-10-23 20:02:06 -0500

    initial commit

---
+++ Descargas/demo(2)/demo/src/main/java/com/aplication/com/DrucSprintApplication.java
@@ -0,0 +1,13 @@
+package com.aplication.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrucSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrucSprintApplication.class, args);
+	}
+
+}
